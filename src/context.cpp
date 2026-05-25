189d71566f5366ca681e6e6bd5588e62fc92bec4
KamilM1205 <bigprogrammer1205@gmail.com>
2026-05-25 21:05:02 +0400

    Feature(context): added interpreter.

--- src/context.cpp
+++ src/context.cpp
@@ -1,16 +1,25 @@
 #include "context.hpp"
+#include "interpreter.hpp"
 #include "message.hpp"
 #include "parser.hpp"
+#include <cstddef>
 
-SLContext::SLContext() {
+SLang::SLContext::SLContext() {
   econ = MessageContainer::get_instance();
   lexer = Lexer();
   parser = Parser();
 }
 
-void SLContext::parse(std::string &source) {
+void SLang::SLContext::parse(std::string &source) {
   econ->set_src(&source);
   lexer.set_source(source);
   lexer.tokenize();
-  parser.parse(&lexer);
+  tree = &parser.parse(&lexer);
+  interpreter.set_tree(tree);
+}
+
+void SLang::SLContext::execute() { interpreter.execute(); }
+
+auto SLang::SLContext::get_global_env() -> Environment & {
+  return interpreter.get_global_env();
 }
--- src/context.hpp
+++ src/context.hpp
@@ -1,19 +1,27 @@
 #ifndef CONTEXT_HPP
 #define CONTEXT_HPP
 
+#include "ast.hpp"
+#include "interpreter.hpp"
 #include "lexer.hpp"
 #include "message.hpp"
 #include "parser.hpp"
 
+namespace SLang {
 class SLContext {
 private:
+  AST::ASTree *tree;
   Lexer lexer;
   Parser parser;
+  Interpreter interpreter;
   MessageContainer *econ;
 
 public:
   SLContext();
   void parse(std::string &source);
+  void execute();
+  auto get_global_env() -> Environment &;
 };
+} // namespace SLang
 
 #endif // CONTEXT_HPP
