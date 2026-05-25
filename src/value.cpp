4ee3e4beb5cca621f216b6768a5139f9241d7e4a
KamilM1205 <bigprogrammer1205@gmail.com>
2026-05-25 20:54:34 +0400

    Feature(value): added SLang representation of value.

---
+++ src/value.cpp
@@ -0,0 +1,323 @@
+#include "value.hpp"
+#include "token.hpp"
+#include "utils.hpp"
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <type_traits>
+#include <variant>
+
+auto SLang::Value::get_number_type(std::string value) -> ValueType {
+  if (value.find('.') != std::string::npos) {
+    return ValueType::FLOAT;
+  }
+
+  return ValueType::INT;
+}
+
+auto SLang::Value::string_to_int(std::string value) -> int32_t {
+  int32_t base = 10;
+
+  if (get_number_type(value) != ValueType::INT) {
+    throw std::runtime_error("Expected int value.");
+  }
+
+  switch (get_base(value)) {
+  case SLang::NumberNotation::BIN:
+    base = 2;
+    break;
+
+  case NumberNotation::ORD:
+    base = 10;
+    break;
+
+  case NumberNotation::HEX:
+    base = 16;
+    break;
+  }
+
+  return std::stoi(get_number_without_notation(value), nullptr, base);
+}
+
+auto SLang::Value::string_to_float(std::string value) -> float {
+  if (get_number_type(value) != ValueType::FLOAT) {
+    throw std::runtime_error("Expected float value.");
+  }
+
+  return std::stof(value);
+}
+
+auto SLang::Value::get_base(std::string value) -> NumberNotation {
+  char notation;
+
+  if (value.size() <= 2) {
+    return NumberNotation::ORD;
+  }
+
+  notation = std::tolower(value[1]);
+
+  if (value[0] == '0') {
+    switch (notation) {
+    case 'b':
+      return NumberNotation::BIN;
+    case 'x':
+      return NumberNotation::HEX;
+    default:
+      return NumberNotation::ORD;
+    }
+  } else {
+    return NumberNotation::ORD;
+  }
+}
+
+// Return string without number notation
+auto SLang::Value::get_number_without_notation(std::string value)
+    -> std::string {
+  if (type == ValueType::INT || type == ValueType::FLOAT) {
+    if (get_base(value) != NumberNotation::ORD) {
+      return value.substr(2);
+    }
+  }
+
+  return value;
+}
+
+auto SLang::Value::storage_to_value_type(Storage &store) -> ValueType {
+  ValueType ret;
+
+  std::visit(type_match{
+                 [&ret](std::monostate) { ret = ValueType::NONE; },
+                 [&ret](AnyType) { ret = ValueType::Any; },
+                 [&ret](int32_t) { ret = ValueType::INT; },
+                 [&ret](float) { ret = ValueType::FLOAT; },
+                 [&ret](bool) { ret = ValueType::BOOL; },
+                 [&ret](std::string) { ret = ValueType::STRING; },
+                 [&ret](std::shared_ptr<Value>) { ret = ValueType::REFERENCE; },
+             },
+             store);
+
+  return ret;
+}
+
+auto SLang::Value::get_data() -> Storage & { return data; }
+
+auto SLang::Value::type_to_string(ValueType type) -> std::string {
+  switch (type) {
+  case ValueType::NONE:
+    return "void";
+  case ValueType::Any:
+    return "any";
+  case ValueType::INT:
+    return "int";
+  case ValueType::FLOAT:
+    return "float";
+  case ValueType::BOOL:
+    return "bool";
+  case ValueType::STRING:
+    return "string";
+  case ValueType::REFERENCE:
+    return "ref";
+  }
+}
+
+auto SLang::Value::type_to_string() -> std::string {
+  return type_to_string(type);
+}
+
+SLang::Value::Value(ValueType type) {
+  this->type = type;
+
+  switch (type) {
+  case ValueType::NONE:
+    data = std::monostate();
+    break;
+  case ValueType::Any:
+    data = AnyType();
+    break;
+  case ValueType::INT:
+    data = 0;
+    break;
+  case ValueType::FLOAT:
+    data = 0.f;
+    break;
+  case ValueType::BOOL:
+    data = false;
+    break;
+  case ValueType::STRING:
+    data = "";
+    break;
+  case ValueType::REFERENCE:
+    data = std::monostate();
+    break;
+  }
+}
+
+SLang::Value::Value(Token token) {
+  switch (token.get_type()) {
+  case TokenType::NUMBER:
+    type = get_number_type(token.get_value());
+    if (type == ValueType::INT) {
+      data = string_to_int(token.get_value());
+    } else {
+      data = string_to_float(token.get_value());
+    }
+    break;
+  case TokenType::LITTERAL:
+    type = ValueType::STRING;
+    data = token.get_value();
+    break;
+  case TokenType::TRUE:
+    type = ValueType::BOOL;
+    data = true;
+    break;
+  case TokenType::FALSE:
+    type = ValueType::BOOL;
+    data = false;
+    break;
+  case TokenType::IDENTIFIER:
+    break;
+  default:
+    throw std::runtime_error("Unexpected type.");
+  }
+}
+
+auto SLang::Value::get_type() -> ValueType { return type; }
+
+auto SLang::Value::from_token_type(SLang::TokenType type) -> ValueType {
+  switch (type) {
+  case TokenType::STRING:
+    return ValueType::STRING;
+  case TokenType::FLOAT:
+    return ValueType::FLOAT;
+  case TokenType::INT:
+    return ValueType::INT;
+  case TokenType::BOOL:
+    return ValueType::BOOL;
+  case TokenType::IDENTIFIER:
+    return ValueType::REFERENCE;
+  default:
+    throw std::runtime_error("Unexpected token type.");
+  }
+}
+
+auto SLang::Value::get_raw_value() -> Value { return get_raw_storage(*this); }
+
+SLang::Value &SLang::Value::operator+(std::string &value) {
+  if (type != ValueType::STRING) {
+    throw std::runtime_error("Expected string value for add.");
+  }
+
+  data = std::get<std::string>(data) + value;
+
+  return *this;
+}
+
+SLang::Value &SLang::Value::operator++() {
+  if (std::holds_alternative<RefType>(data)) {
+    std::visit(
+        [](auto &d) {
+          using T = std::decay_t<decltype(d)>;
+          if constexpr (!can_increment<T>) {
+            throw std::runtime_error(std::format(
+                "Cannot increment given type: {}", typeid(T).name()));
+          } else {
+            d++;
+          }
+        },
+        get_last_ref(std::get<RefType>(data))->data);
+  } else {
+    throw std::runtime_error("Expected numerical value for increment.");
+  }
+
+  return *this;
+}
+
+SLang::Value &SLang::Value::operator--() {
+  if (std::holds_alternative<RefType>(data)) {
+    std::visit(
+        [](auto &d) {
+          using T = std::decay_t<decltype(d)>;
+          if constexpr (!can_decrement<T>) {
+            throw std::runtime_error(std::format(
+                "Cannot decrement given type: {}", typeid(T).name()));
+          } else {
+            d--;
+          }
+        },
+        get_last_ref(std::get<RefType>(data))->data);
+  } else {
+    throw std::runtime_error("Expected numerical value for increment.");
+  }
+
+  return *this;
+}
+
+SLang::Value &SLang::Value::operator++(int) {
+  Value value = *this;
+  ++(*this);
+  return *this;
+}
+
+SLang::Value &SLang::Value::operator--(int) {
+  Value value = *this;
+  --(*this);
+  return *this;
+}
+
+SLang::Value &SLang::Value::operator=(const Value &value) {
+  if (std::holds_alternative<RefType>(data)) {
+    auto l = get_last_ref(std::get<RefType>(data));
+    Storage r = value.data;
+
+    if (std::holds_alternative<RefType>(value.data)) {
+      r = get_last_ref(std::get<RefType>(value.data));
+    }
+
+    std::visit(
+        [](auto &lhs, auto &rhs) {
+          if constexpr (requires { lhs = rhs; }) {
+            lhs = rhs;
+          } else {
+            throw std::runtime_error(
+                std::format("Cannot assign value with type: {}, to {}",
+                            typeid(rhs).name(), typeid(lhs).name()));
+          }
+        },
+        l->data, r);
+  } else {
+    type = value.type;
+    std::visit(
+        [](auto &lhs, auto &rhs) {
+          if constexpr (requires { lhs = rhs; }) {
+            lhs = rhs;
+          } else {
+            throw std::runtime_error(
+                std::format("Cannot assign value with type: {}, to {}",
+                            typeid(rhs).name(), typeid(lhs).name()));
+          }
+        },
+        data, value.data);
+  }
+
+  return *this;
+}
+
+auto SLang::Value::to_string() const -> const std::string {
+  std::string out;
+
+  if (std::holds_alternative<int32_t>(data)) {
+    out = std::to_string(std::get<int32_t>(data));
+  } else if (std::holds_alternative<float>(data)) {
+    out = std::to_string(std::get<float>(data));
+  } else if (std::holds_alternative<bool>(data)) {
+    out = std::get<bool>(data) ? "true" : "false";
+  } else if (std::holds_alternative<std::string>(data)) {
+    out = std::get<std::string>(data);
+  } else if (std::holds_alternative<RefType>(data)) {
+    out = std::get<std::shared_ptr<Value>>(data)->to_string();
+  } else {
+    out = "error";
+  }
+
+  return out;
+}
--- src/value.hpp
+++ src/value.hpp
@@ -1,63 +1,195 @@
 #ifndef VALUE_HPP
 #define VALUE_HPP
 
-#include "lexer.hpp"
-#include <atomic>
+#include "token.hpp"
 #include <cctype>
 #include <cstddef>
+#include <format>
+#include <functional>
+#include <memory>
+#include <source_location>
+#include <stdexcept>
 #include <string>
 #include <type_traits>
+#include <variant>
 
 namespace SLang {
-template <typename T> class Value {
-protected:
-  std::atomic<size_t> _id;
 
+enum class ValueType {
+  NONE,
+  Any,
+  INT,
+  FLOAT,
+  BOOL,
+  STRING,
+  REFERENCE,
+};
+
+class Value {
 private:
-  T data;
+  using RefType = std::shared_ptr<Value>;
+  struct AnyType {};
+  using Storage = std::variant<std::monostate, AnyType, int32_t, float,
+                               std::string, bool, RefType>;
 
-  auto get_base(std::string value) -> NumberNotation {
-    char notation;
+  Storage data;
+  ValueType type;
 
-    if (value.size() <= 2) {
-      return NumberNotation::ORD;
-    }
+  auto get_number_type(std::string value) -> ValueType;
+  auto string_to_int(std::string value) -> int32_t;
+  auto string_to_float(std::string value) -> float;
 
-    notation = std::tolower(value[1]);
+  auto get_base(std::string value) -> NumberNotation;
+  // Return string without number notation
+  auto get_number_without_notation(std::string value) -> std::string;
 
-    if (value[0] == '0') {
-      switch (notation) {
-      case 'b':
-        return NumberNotation::BIN;
-      case 'f':
-        return NumberNotation::HEX;
-      default:
-        return NumberNotation::ORD;
+  auto storage_to_value_type(Storage &store) -> ValueType;
+
+  template <typename T> static Storage get_raw_storage(T value) {
+    if constexpr (std::is_same_v<T, Storage>) {
+      if (std::holds_alternative<RefType>(value)) {
+        return get_raw_storage(std::get<RefType>(value)->data);
+      } else {
+        return value;
       }
+    } else if constexpr (std::is_same_v<T, RefType>) {
+      return get_raw_storage(value->data);
+    } else if constexpr (std::is_same_v<T, Value>) {
+      return get_raw_storage(value.data);
+    } else {
+      return value;
     }
   }
 
-  // Return string without number notation
-  auto get_no_notation(std::string value) -> std::string {}
-
-  void str2type(std::string value) {
-    if constexpr (std::is_same_v<T, int>) {
-      data = std::stoi(get_no_notation(value), nullptr, get_base(value));
-    } else if constexpr (std::is_same_v<T, float>) {
-
-    } else if constexpr (std::is_same_v<T, std::string>) {
-
-    } else {
+  static RefType get_last_ref(const RefType &value) {
+    if (std::holds_alternative<RefType>(value->data)) {
+      return get_last_ref(std::get<RefType>(value->data));
     }
+
+    return value;
   }
 
 public:
-  Value() : _id(_id++) {}
-  Value(T value) : _id(_id++) { data = value; }
-  Value(std::string value) : _id(_id++) {}
-  Value(Value &value) : _id(_id++), data(value.data) {}
+  Value() : data(std::monostate()), type(ValueType::NONE) {};
+  Value(ValueType type);
+  Value(Token token);
+  Value(int32_t value) : data(value), type(ValueType::INT) {}
+  Value(float value) : data(value), type(ValueType::FLOAT) {}
+  Value(bool value) : data(value), type(ValueType::BOOL) {}
+  Value(std::string value) : data(value), type(ValueType::STRING) {}
+  Value(RefType value) : data(value), type(ValueType::REFERENCE) {}
+  Value(Storage store) : data(store), type(storage_to_value_type(store)) {}
 
-  auto id() -> size_t { return _id; }
+  [[nodiscard]]
+  auto get_type() -> ValueType;
+
+  [[nodiscard]]
+  auto get_data() -> Storage &;
+
+  [[nodiscard]]
+  static auto type_to_string(ValueType type) -> std::string;
+
+  [[nodiscard]]
+  auto type_to_string() -> std::string;
+
+  [[nodiscard]]
+  static auto from_token_type(TokenType type) -> ValueType;
+
+  [[nodiscard]]
+  auto get_raw_value() -> Value;
+
+  template <typename T, typename F>
+  friend T make_operation(
+      F op, const Value &lhs, const Value &rhs,
+      const std::source_location &loc = std::source_location::current()) {
+    auto l = get_raw_storage(lhs);
+    auto r = get_raw_storage(rhs);
+
+    return std::visit(
+        [&loc, &op](auto &lhs, auto &rhs) -> T {
+          if constexpr (requires { op(lhs, rhs); }) {
+            return op(lhs, rhs);
+          } else {
+            throw std::runtime_error(std::format(
+                "Cannot apply operation: {} for lhs: {} and rhs: {}",
+                loc.function_name(), typeid(lhs).name(), typeid(rhs).name()));
+          }
+        },
+        l, r);
+  }
+
+  friend Value operator+(const Value &lhs, const Value &rhs) {
+    auto l = get_raw_storage(lhs);
+
+    if (std::holds_alternative<std::string>(l)) {
+      return std::get<std::string>(l) + rhs.to_string();
+    } else {
+      return make_operation<Value>(std::plus{}, lhs, rhs);
+    }
+  }
+
+  Value &operator+(std::string &value);
+
+  friend Value operator-(const Value &lhs, const Value &rhs) {
+    return make_operation<Value>(std::minus{}, lhs, rhs);
+  }
+
+  friend Value operator*(const Value &lhs, const Value &rhs) {
+    return make_operation<Value>(std::multiplies{}, lhs, rhs);
+  }
+
+  friend Value operator/(const Value &lhs, const Value &rhs) {
+    return make_operation<Value>(std::divides{}, lhs, rhs);
+  }
+
+  friend Value operator%(const Value &lhs, const Value &rhs) {
+    return make_operation<Value>(std::modulus{}, lhs, rhs);
+  }
+
+  friend bool operator!=(const Value &lhs, const Value &rhs) {
+    return make_operation<bool>(std::not_equal_to{}, lhs, rhs);
+  }
+
+  friend bool operator==(const Value &lhs, const Value &rhs) {
+    return make_operation<bool>(std::equal_to{}, lhs, rhs);
+  }
+
+  friend bool operator<(const Value &lhs, const Value &rhs) {
+    return make_operation<bool>(std::less{}, lhs, rhs);
+  }
+
+  friend bool operator<=(const Value &lhs, const Value &rhs) {
+    return make_operation<bool>(std::less_equal{}, lhs, rhs);
+  }
+
+  friend bool operator>(const Value &lhs, const Value &rhs) {
+    return make_operation<bool>(std::greater{}, lhs, rhs);
+  }
+
+  friend bool operator>=(const Value &lhs, const Value &rhs) {
+    return make_operation<bool>(std::greater_equal{}, lhs, rhs);
+  }
+
+  Value &operator=(const Value &value);
+
+  Value &operator++();
+  Value &operator--();
+  Value &operator++(int);
+  Value &operator--(int);
+
+  template <typename T>
+  [[nodiscard]]
+  T get_raw() {
+    if (std::holds_alternative<T>(data)) {
+      return std::get<T>(data);
+    } else {
+      throw std::runtime_error(std::format(
+          "Error: cannot get raw value with type: {}", typeid(T).name()));
+    }
+  }
+
+  [[nodiscard]]
+  auto to_string() const -> const std::string;
 };
 } // namespace SLang
 
