5c4ece4277f4f7513dbf0dcdd7eb428d1d19168b
KamilM1205 <bigprogrammer1205@gmail.com>
2026-05-25 21:04:19 +0400

    Feature(utils): added some helper functions for interpreter and value.

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,6 +1,10 @@
 #ifndef UTILS_HPP
 #define UTILS_HPP
 
+#include <cassert>
+#include <iostream> // IWYU pragma: keep
+#include <type_traits>
+
 #define cross_inline __attribute__((always_inline)) inline
 
 /*
@@ -15,5 +19,34 @@ protected:                                                                     \
                                                                                \
 public:                                                                        \
   type &name() { return _##name; }
+
+namespace SLang {
+/*
+ * @brief Match-like для std::visit
+ */
+template <class... Ts> struct type_match : Ts... {
+  using Ts::operator()...;
+};
+
+template <typename> struct always_false : std::false_type {};
+
+template <typename T> constexpr bool always_false_v = always_false<T>::value;
+
+template <typename T>
+concept can_increment = requires(T val) { val++; };
+
+template <typename T>
+concept can_decrement = requires(T val) { val--; };
+
+template <typename T> struct TypeError;
+
+#define SLAssert(cond, msg)                                                    \
+  do {                                                                         \
+    if (!(cond)) {                                                             \
+      std::cerr << msg << std::endl;                                           \
+      std::abort();                                                            \
+    }                                                                          \
+  } while (0)
+} // namespace SLang
 
 #endif // !UTILS_HPP
